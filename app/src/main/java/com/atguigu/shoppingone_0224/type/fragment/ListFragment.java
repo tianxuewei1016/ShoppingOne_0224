4838095d1c24f7c2db2c6aa4444bbebe26734631
tianxuewei1016 <93226539@qq.com>
2017-06-16 18:34:08 +0800

    联网请求数据

--- app/src/main/java/com/atguigu/shoppingone_0224/type/fragment/ListFragment.java
+++ app/src/main/java/com/atguigu/shoppingone_0224/type/fragment/ListFragment.java
@@ -1,16 +1,23 @@
 package com.atguigu.shoppingone_0224.type.fragment;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.alibaba.fastjson.JSON;
 import com.atguigu.shoppingone_0224.R;
 import com.atguigu.shoppingone_0224.base.BaseFragment;
 import com.atguigu.shoppingone_0224.type.adapter.TypeLeftAdapter;
+import com.atguigu.shoppingone_0224.type.bean.TypeBean;
+import com.atguigu.shoppingone_0224.utils.Constants;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import okhttp3.Call;
 
 /**
  * 作者：田学伟 on 2017/6/16 15:56
@@ -19,7 +26,7 @@ import butterknife.InjectView;
  */
 
 public class ListFragment extends BaseFragment {
-
+    private static final String TAG = ListFragment.class.getSimpleName();
     @InjectView(R.id.lv_left)
     ListView lvLeft;
     @InjectView(R.id.rv_right)
@@ -28,7 +35,9 @@ public class ListFragment extends BaseFragment {
     //网络请求得到数据
     private String[] titles = new String[]{"小裙子", "上衣", "下装", "外套", "配件", "包包", "装扮", "居家宅品",
             "办公文具", "数码周边", "游戏专区"};
-
+    private String[] urls = new String[]{Constants.SKIRT_URL, Constants.JACKET_URL, Constants.PANTS_URL, Constants.OVERCOAT_URL,
+            Constants.ACCESSORY_URL, Constants.BAG_URL, Constants.DRESS_UP_URL, Constants.HOME_PRODUCTS_URL, Constants.STATIONERY_URL,
+            Constants.DIGIT_URL, Constants.GAME_URL};
     private TypeLeftAdapter typeLeftAdapter;
 
     @Override
@@ -56,8 +65,40 @@ public class ListFragment extends BaseFragment {
                 typeLeftAdapter.changeSelectPosition(position);
                 //2.刷新适配器
                 typeLeftAdapter.notifyDataSetChanged();//getView
+
+                //联网请求
+                getDataFromNet(urls[position]);
             }
         });
+        getDataFromNet(urls[0]);
+    }
+
+    /**
+     * 网络请求
+     *
+     * @param url
+     */
+    private void getDataFromNet(String url) {
+        OkHttpUtils.get()
+                .url(url)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.e(TAG, "请求成功失败==" + e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.e(TAG, "请求成功==" + response);
+                        processData(response);
+                    }
+                });
+    }
+
+    private void processData(String json) {
+        TypeBean typeBean = JSON.parseObject(json, TypeBean.class);
+        Log.e("TAG", "解析成功==" + typeBean.getResult().get(0).getName());
     }
 
     @Override
